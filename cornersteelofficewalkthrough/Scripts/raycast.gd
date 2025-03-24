3c8bc956337744bfb00b5995798f1f7dff1bafc9
Dumaraos, Mark Joseph <dumaraosmark112@gmail.com>
2025-03-24 17:44:32 +0800

    Up

--- cornersteelofficewalkthrough/Scripts/raycast.gd
+++
@@ -1,8 +0,0 @@
-extends RayCast3D
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if is_colliding():
-		var hitObj = get_collider()
-		if hitObj.has_method("interact") and (Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("controller_interact") or Input.is_action_just_pressed("click_interact") or Input.is_action_just_pressed("F_interact")):
-			hitObj.interact()
